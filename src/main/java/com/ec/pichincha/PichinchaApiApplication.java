7f83ded46f5c645a9981ee014f7cd2794c6392d8
klever <klever.epn@gmail.com>
2020-08-31 21:44:45 -0500

    mock test login

--- src/main/java/com/ec/pichincha/PichinchaApiApplication.java
+++ src/main/java/com/ec/pichincha/PichinchaApiApplication.java
@@ -1,13 +1,22 @@
 package com.ec.pichincha;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.ec.pichincha")
 @SpringBootApplication
-public class PichinchaApiApplication {
+public class PichinchaApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PichinchaApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- src/main/java/com/ec/pichincha/SpringSecurityConfig.java
+++ src/main/java/com/ec/pichincha/SpringSecurityConfig.java
@@ -1,19 +1,20 @@
 package com.ec.pichincha;
 
-import com.ec.pichincha.filters.JwtFilter;
-import com.ec.pichincha.filters.AuthFilter;
-import com.ec.pichincha.auth.services.IJwtService;
-import org.springframework.context.annotation.Bean;
-import com.ec.pichincha.services.UserDetailServiceImpl;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.ec.pichincha.auth.services.IJwtService;
+import com.ec.pichincha.filters.AuthFilter;
+import com.ec.pichincha.filters.JwtFilter;
+import com.ec.pichincha.services.UserDetailServiceImpl;
 
 /**
  * Spring security config with token library
@@ -25,7 +26,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private UserDetailServiceImpl userDetaisService;
+	private UserDetailServiceImpl userDetailsService;
 
 	@Autowired
 	private IJwtService jwtService;
@@ -56,7 +57,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// always do comparison with encripted password 
-		auth.userDetailsService(this.userDetaisService).passwordEncoder(bcrypt);
+		auth.userDetailsService(this.userDetailsService).passwordEncoder(bcrypt);
 	}
 
 }
