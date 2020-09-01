04ae75b06333bb15f35b7b501930d6e5e555897e
klever <klever.epn@gmail.com>
2020-09-01 14:43:36 -0500

    second test login

---
+++ src/test/java/com/ec/pichincha/auth/AuthorizationControllerTest.java
@@ -0,0 +1,67 @@
+package com.ec.pichincha.auth;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.ec.pichincha.vos.UserVo;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthorizationControllerTest {
+
+	@Autowired
+	private MockMvc mock;
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	@Test
+	public void login() throws Exception {
+		String uri = "/auth/login";
+		UserVo user = new UserVo();
+		user.setUsername("admin");
+		user.setPassword("123");
+
+		String inputJson = mapToJson(user);
+
+		MvcResult mvcResult = this.mock.perform(
+				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+	}
+
+	public void setUp() {
+		this.mock = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	public String mapToJson(Object obj) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(obj);
+	}
+
+	public <T> T mapFromJson(String json, Class<T> clazz)
+			throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, clazz);
+	}
+
+}
