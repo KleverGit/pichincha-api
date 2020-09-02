9150da6a259ec656b64c57ba91e65f8fc6346c48
klever <klever.epn@gmail.com>
2020-09-01 21:12:16 -0500

    develop config yaml circleci test3

--- .circleci/gcloud.sh
+++ .circleci/gcloud.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 set -e
+
 # setting up environment dependent variables
-ENVIRONMENT=${1:-TEST}
-GCLOUD_SERVICE_KEY=$(eval echo "\$GCLOUD_SERVICE_KEY_$ENVIRONMENT")
-GCLOUD_PROJECT=$(eval echo "\$GCLOUD_PROJECT_$ENVIRONMENT")
+ENV=${1:-TEST}
+GCLOUD_SERVICE_KEY=$(eval echo "\$GCLOUD_SERVICE_KEY_$ENV")
+GCLOUD_PROJECT=$(eval echo "\$GCLOUD_PROJECT_$ENV")
 # expecting the install directly in the home directory
 GCLOUD=${HOME}/google-cloud-sdk/bin/gcloud
+
 echo ${GCLOUD_SERVICE_KEY} | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
 
 ${GCLOUD} auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
